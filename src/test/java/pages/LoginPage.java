f10118356430961db457b72b3c77da48324811b7
cll2 <75150662+cll2@users.noreply.github.com>
2024-08-16 22:31:00 -0400

    uncommented login tests

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -3,6 +3,7 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import pages.BasePage;
 
 public class LoginPage extends BasePage {
 
